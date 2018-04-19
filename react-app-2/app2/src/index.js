f935db9293a30eaf05c3a034079120bdd11a4b27
Panchenko Svetlana <ivanovasvetlana1991@gmail.com>
2018-04-20 01:47:51 +0400

    временный коммит

--- react-app-2/app2/src/index.js
+++ react-app-2/app2/src/index.js
@@ -1,8 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { AppContainer } from 'react-hot-loader';
+
+
+import configureStore from './store';
+import Root from "./root.component";
+
+const store = configureStore();
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer warnings={false}>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('app2')
+  );
+};
+
+render(Root);
+//
+// if (module.hot) {
+//   module.hot.accept('./components/Routes', () => {
+//     // eslint-disable-next-line
+//     const NextRoutes = require('./components/Routes').default;
+//
+//     render(NextRoutes);
+//   });
+// }
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import singleSpaReact from 'single-spa-react';
+// import Root from './root.component.js';
+//
+// const reactLifecycles = singleSpaReact({
+//   React,
+//   ReactDOM,
+//   rootComponent: Root,
+//   domElementGetter,
+// });
+//
+// export function bootstrap(props) {
+//   return reactLifecycles.bootstrap(props);
+// }
+//
+// export function mount(props) {
+//   return reactLifecycles.mount(props);
+// }
+//
+// export function unmount(props) {
+//   return reactLifecycles.unmount(props);
+// }
+//
+// function domElementGetter() {
+//   // Make sure there is a div for us to render into
+//   let el = document.getElementById('app2');
+//   if (!el) {
+//     el = document.createElement('div');
+//     el.id = 'app2';
+//     document.body.appendChild(el);
+//   }
+//
+//   return el;
+// }
